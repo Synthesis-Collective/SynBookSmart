78bf187c8215b7e5d0c4fc494eab370fd095132d
KJack <kgithub@kellerg.com>
2021-05-08 01:25:44 -0500

    Add labels for Map Markers and Quests - New Feature: label books that create Map Markers - New Feature: label books tied to a quest (detected multiple ways) - Multiple tags are handled gracefully

--- BookSmart/Program.cs
+++ BookSmart/Program.cs
@@ -1,25 +1,26 @@
 using System;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Converters;
-using System.Text.RegularExpressions;
 using Noggog;
 
 namespace BookSmart
 {
-    // Sorry, I don't really know what I'm doing. Stop judging me.
     public class Program
     {
-
+        // Settings
         static Lazy<Settings> LazySettings = new Lazy<Settings>();
         static Settings settings => LazySettings.Value;
-
+        
+        // Initial setup
         public static async Task<int> Main(string[] args)
         {
             return await SynthesisPipeline.Instance
@@ -33,105 +34,275 @@ namespace BookSmart
                 .Run(args);
         }
 
+        // Let's get to work!
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            state.LoadOrder.PriorityOrder.OnlyEnabled().Book().WinningOverrides().ForEach(book =>
+            // If quest labels are enabled, create the Quest Book cache first
+            List<String> questBookCache = new();
+            if (settings.addQuestLabels) { questBookCache = CreateQuestBookCache(state); }
+            
+            // Iterate all winning books from the load order
+            foreach (var book in state.LoadOrder.PriorityOrder.OnlyEnabled().Book().WinningOverrides())
             {
-                if (book.Teaches is IBookSkillGetter skillTeach)
+                // If the book has no name, skip it
+                if (book.Name == null) { continue; }
+
+                // Store our new tags
+                List<String> newTags = new();
+
+                // Add Skill labels
+                if (settings.addSkillLabels && book.Teaches is IBookSkillGetter skillTeach)
                 {
-                    // variables for use in this section
-                    string newName = "Unknown";
-                    string? skillName = "Unknown";
-                    string open = "";
-                    string close = "";
-
-                    // set the open and close characters that go around the skill name
-                    switch (settings.encapsulatingCharacters)
-                    {
-                        case Settings.EncapsulatingCharacters.Chevrons: { open = "<"; close = ">"; break; }
-                        case Settings.EncapsulatingCharacters.Curly_Brackets: { open = "{"; close = "}"; break; }
-                        case Settings.EncapsulatingCharacters.Parenthesis: { open = "("; close = ")"; break; }
-                        case Settings.EncapsulatingCharacters.Square_Brackets: { open = "["; close = "]"; break; }
-                        case Settings.EncapsulatingCharacters.Stars: { open = "*"; close = "*"; break; }
-                        default: throw new NotImplementedException("Somehow you set Encapsulating Characters to something that isn't supported.");
-                    }
-
-                    // Label Format: Long
-                    if (settings.labelFormat == Settings.LabelFormat.Long)
-                    {
-                        skillName = skillTeach.Skill switch
-                        {
-                            Skill.HeavyArmor => "Heavy Armor",
-                            Skill.LightArmor => "Light Armor",
-                            Skill.OneHanded => "One-Handed",
-                            Skill.TwoHanded => "Two-Handed",
-                            _ => skillTeach.Skill.ToString()
-                        };
-
-                        switch (settings.labelPosition)
-                        {
-                            case Settings.LabelPosition.Before_Name: { newName = $"{open}{skillName}{close} {book.Name}"; break; }
-                            case Settings.LabelPosition.After_Name: { newName = $"{book.Name} {open}{skillName}{close}"; break; }
-                            default: throw new NotImplementedException("Somehow your set Label Position to something that isn't supported.");
-                        }
-                        
-                    }
-                    // Label Format: Short
-                    else if (settings.labelFormat == Settings.LabelFormat.Short)
-                    {
-                        skillName = skillTeach.Skill switch
-                        {
-                            Skill.Alchemy => "Alch",
-                            Skill.Alteration => "Altr",
-                            Skill.Archery => "Arch",
-                            Skill.Block => "Blck",
-                            Skill.Conjuration => "Conj",
-                            Skill.Destruction => "Dest",
-                            Skill.Enchanting => "Ench",
-                            Skill.HeavyArmor => "H.Arm",
-                            Skill.Illusion => "Illu",
-                            Skill.LightArmor => "L.Arm",
-                            Skill.Lockpicking => "Lock",
-                            Skill.OneHanded => "1H",
-                            Skill.Pickpocket => "Pick",
-                            Skill.Restoration => "Resto",
-                            Skill.Smithing => "Smith",
-                            Skill.Sneak => "Snk",
-                            Skill.Speech => "Spch",
-                            Skill.TwoHanded => "2H",
-                            _ => skillTeach.Skill.ToString()
-                        };
-
-                        switch (settings.labelPosition)
-                        {
-                            case Settings.LabelPosition.Before_Name: { newName = $"{open}{skillName}{close} {book.Name}"; break; }
-                            case Settings.LabelPosition.After_Name: { newName = $"{book.Name} {open}{skillName}{close}"; break; }
-                            default: throw new NotImplementedException("Somehow your set Label Position to something that isn't supported.");
-                        }
-                    }
-                    // Label Format: Star
-                    else if (settings.labelFormat == Settings.LabelFormat.Star)
-                    {
-                        switch (settings.labelPosition)
-                        {
-                            case Settings.LabelPosition.Before_Name: { newName = $"*{book.Name}"; break; }
-                            case Settings.LabelPosition.After_Name: { newName = $"{book.Name}*"; break; }
-                            default: throw new NotImplementedException("Somehow your set Label Position to something that isn't supported.");
-                        }
-                    }
-                    else
-                    {
-                        throw new NotImplementedException("Somehow you set labelFormat to something that isn't supported.");
-                    }
-
-                    // Actually create the override record
-                    var bookOverride = state.PatchMod.Books.GetOrAddAsOverride(book);
-                    bookOverride.Name = newName;
-
-                    // Console output
-                    Console.WriteLine($"{book.FormKey}: '{book.Name}' -> '{bookOverride.Name}'");
+                    var skillLabel = GetSkillLabelName(book);
+                    if (skillLabel is not null) { newTags.Add(skillLabel); }
                 }
-            });
+
+                //Add Map Marker labels
+                if (settings.addMapMarkerLabels && (book.VirtualMachineAdapter is not null && book.VirtualMachineAdapter.Scripts.Count > 0)                    )
+                {
+                    var mapMarkerLabel = GetMapLabelName(book);
+                    if (mapMarkerLabel is not null) { newTags.Add(mapMarkerLabel); }
+                }
+
+                // Add Quest labels
+                if (settings.addQuestLabels)
+                {
+                    var questLabel = GetQuestLabelName(book, questBookCache);
+                    if (questLabel is not null) { newTags.Add(questLabel); }
+                }
+
+                // If we don't have any new tags, no need for an override record
+                if (newTags.Count == 0) { continue; }
+
+                // Actually create the override record
+                var bookOverride = state.PatchMod.Books.GetOrAddAsOverride(book);
+                
+                // Special handling for a labelFormat of Star
+                if (settings.labelFormat == Settings.LabelFormat.Star)
+                {
+                    switch (settings.labelPosition) {
+                        case Settings.LabelPosition.Before_Name: { bookOverride.Name = $"*{book.Name.ToString()}"; break; }
+                        case Settings.LabelPosition.After_Name: { bookOverride.Name = $"{book.Name.ToString()}*"; break; }
+                        default: throw new NotImplementedException("Somehow your set Label Position to something that isn't supported.");
+                    }
+                }
+                // All other labelFormats
+                else
+                {
+                    bookOverride.Name = GetLabel(book.Name.ToString()!, String.Join("/", newTags));
+                }
+
+                // Console output
+                Console.WriteLine($"{book.FormKey}: '{book.Name}' -> '{bookOverride.Name}'");
+            };
         }
+
+        
+        public static string GetLabel(string existingName, string newLabel)
+        {
+            // set the open and close characters that go around the skill name
+            string open = "";
+            string close = "";
+            switch (settings.encapsulatingCharacters)
+            {
+                case Settings.EncapsulatingCharacters.Chevrons: { open = "<"; close = ">"; break; }
+                case Settings.EncapsulatingCharacters.Curly_Brackets: { open = "{"; close = "}"; break; }
+                case Settings.EncapsulatingCharacters.Parenthesis: { open = "("; close = ")"; break; }
+                case Settings.EncapsulatingCharacters.Square_Brackets: { open = "["; close = "]"; break; }
+                case Settings.EncapsulatingCharacters.Stars: { open = "*"; close = "*"; break; }
+                default: throw new NotImplementedException("Somehow you set Encapsulating Characters to something that isn't supported.");
+            }
+
+            return settings.labelPosition switch
+            {
+                Settings.LabelPosition.Before_Name => $"{open}{newLabel}{close} {existingName}",
+                Settings.LabelPosition.After_Name => $"{existingName} {open}{newLabel}{close}",
+                _ => throw new NotImplementedException("Somehow your set Label Position to something that isn't supported.")
+            };
+        }
+        
+        public static List<string> CreateQuestBookCache(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("Flipping through the quest library looking for books, please wait...");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            List<string> questBookCache = new List<String>();
+
+            // Search all quests
+            foreach (var quest in state.LoadOrder.PriorityOrder.OnlyEnabled().Quest().WinningOverrides())
+            {
+                if ((quest.Aliases is not null) && (quest.Aliases.Count > 0))
+                {
+                    // Examine each alias
+                    foreach (var alias in quest.Aliases)
+                    {
+                        // CreateReferenceToObject alias
+                        if (alias.CreateReferenceToObject is not null)
+                        {
+                            // try to resolve the quest object to the actual records
+                            if (!alias.CreateReferenceToObject.Object.TryResolve(state.LinkCache, out var questObject))
+                            {
+                                Console.WriteLine($"WARNING: Referenced object '{alias.CreateReferenceToObject.Object}' in Quest '{quest.FormKey} could not be resolved.");
+                            }
+                            else if (questObject is IBookGetter)
+                            {
+                                //Console.WriteLine($"{quest.FormKey}: '{questObject.FormKey}' is used in quest '{quest.Name}'");
+                                questBookCache.Add(questObject.FormKey.ToString());
+                            }
+                        }
+
+                        // Items alias
+                        if (alias.Items is not null)
+                        {
+                            // try to resolve the quest object ot the actual records
+                            foreach (var item in alias.Items)
+                            {
+                                // try to resolve the quest object to the actual records
+                                // item.item.item.item
+                                if (!item.Item.Item.TryResolve(state.LinkCache, out var questObject))
+                                {
+                                    Console.WriteLine($"WARNING: Referenced object '{item.Item.Item}' in Quest '{quest.FormKey} could not be resolved.");
+                                }
+                                else if (questObject is IBookGetter)
+                                {
+                                    //Console.WriteLine($"{quest.FormKey}: '{questObject.FormKey}' is used in quest '{quest.Name}'");
+                                    questBookCache.Add(questObject.FormKey.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return questBookCache;
+        }
+
+
+        public static string? GetSkillLabelName(IBookGetter book)
+        {
+            // variables for use in this section
+            if (book.Teaches == null) { return null; }
+            IBookSkillGetter skillTeach = (IBookSkillGetter)book.Teaches;
+            if (skillTeach == null || skillTeach.Skill == null) { return null; }
+
+            // Label Format: Long
+            if (settings.labelFormat == Settings.LabelFormat.Long)
+            {
+                return skillTeach.Skill switch
+                {
+                    Skill.HeavyArmor => "Heavy Armor",
+                    Skill.LightArmor => "Light Armor",
+                    Skill.OneHanded => "One-Handed",
+                    Skill.TwoHanded => "Two-Handed",
+                    _ => skillTeach.Skill.ToString()
+                };
+            }
+            // Label Format: Short
+            else if (settings.labelFormat == Settings.LabelFormat.Short)
+            {
+                return skillTeach.Skill switch
+                {
+                    Skill.Alchemy => "Alch",
+                    Skill.Alteration => "Altr",
+                    Skill.Archery => "Arch",
+                    Skill.Block => "Blck",
+                    Skill.Conjuration => "Conj",
+                    Skill.Destruction => "Dest",
+                    Skill.Enchanting => "Ench",
+                    Skill.HeavyArmor => "H.Arm",
+                    Skill.Illusion => "Illu",
+                    Skill.LightArmor => "L.Arm",
+                    Skill.Lockpicking => "Lock",
+                    Skill.OneHanded => "1H",
+                    Skill.Pickpocket => "Pick",
+                    Skill.Restoration => "Resto",
+                    Skill.Smithing => "Smith",
+                    Skill.Sneak => "Snk",
+                    Skill.Speech => "Spch",
+                    Skill.TwoHanded => "2H",
+                    _ => skillTeach.Skill.ToString()
+                };
+            }
+            // Label Format: Star
+            else if (settings.labelFormat == Settings.LabelFormat.Star)
+            {
+                return "*";
+            }
+            else
+            {
+                throw new NotImplementedException("Somehow you set labelFormat to something that isn't supported.");
+            }
+        }
+
+
+        public static string? GetMapLabelName(IBookGetter book)
+        {
+            // variables for use in this section
+            if (book == null) { return null; }
+            if (book.VirtualMachineAdapter == null) { return null; }
+
+            foreach (var script in book.VirtualMachineAdapter.Scripts)
+            {
+                // Any script with MapMarker in the script name will do
+                if (script.Name.Contains("MapMarker", StringComparison.OrdinalIgnoreCase))
+                {
+                    return settings.labelFormat switch
+                    {
+                        Settings.LabelFormat.Long => "Map Marker",
+                        Settings.LabelFormat.Short => "Map",
+                        Settings.LabelFormat.Star => "*",
+                        _ => throw new NotImplementedException("Somehow you set labelFormat to something that isn't supported.")
+                    };
+                }
+            }
+
+            return null;
+        }
+
+
+        public static string? GetQuestLabelName(IBookGetter book, List<string> questBookCache)
+        {
+            bool isBookQuestRealted = false;
+
+            // Check the Quest Book Cache
+            
+            if (questBookCache.Contains(book.FormKey.ToString()))
+            {
+                isBookQuestRealted = true;
+            }
+            // Check for quest-related Book scripts
+            else if ((book.VirtualMachineAdapter is not null) &&
+                    (book.VirtualMachineAdapter.Scripts is not null) &&
+                    (book.VirtualMachineAdapter.Scripts.Count > 0))
+            {
+                foreach (var script in book.VirtualMachineAdapter.Scripts)
+                {
+                    if (script.Name.Contains("Quest", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine($"{book.FormKey}: '{book.Name}' has a quest script called '{script.Name}'.");
+                        isBookQuestRealted = true;
+                    }
+                }
+            }
+
+            if (isBookQuestRealted)
+            {
+                return settings.labelFormat switch
+                {
+                    Settings.LabelFormat.Long => "Quest",
+                    Settings.LabelFormat.Short => "Q",
+                    Settings.LabelFormat.Star => "*",
+                    _ => throw new NotImplementedException("Somehow you set labelFormat to something that isn't supported.")
+                };
+            } else
+            {
+                return null;
+            }
+            
+        }
+
+
+        
     }
 }
--- BookSmart/Settings.cs
+++ BookSmart/Settings.cs
@@ -12,6 +12,31 @@ namespace BookSmart
 {
     class Settings
     {
+        // Which labels to add
+        [SynthesisOrder]
+        [SynthesisTooltip("Add a skill tag to the book name if the book teaches a skill.")]
+        public bool addSkillLabels = true;
+        
+        [SynthesisOrder]
+        [SynthesisTooltip("Add a map marker tag to the book name if the book teaches map markers.")]
+        public bool addMapMarkerLabels = true;
+
+        [SynthesisOrder]
+        [SynthesisTooltip("Add a quest tag to the book name if the book is used in a quest.")]
+        public bool addQuestLabels = true;
+
+        // Label Position
+        public enum LabelPosition
+        {
+            Before_Name,
+            After_Name
+        }
+
+        [SynthesisOrder]
+        [SynthesisSettingName("Label Position")]
+        [SynthesisTooltip("Where to put the label when creating the new name.")]
+        public LabelPosition labelPosition { get; set; } = LabelPosition.Before_Name;
+
         // Label Format
         public enum LabelFormat
         {
@@ -39,17 +64,5 @@ namespace BookSmart
         [SynthesisSettingName("Encapsulating Characters")]
         [SynthesisTooltip("The characters to wrap the skill name in.\r\nParenthesis: ()\r\nCurly Brackets: {}\r\nSquare Brackets: []\r\nChevrons: <>\r\nStars: *")]
         public EncapsulatingCharacters encapsulatingCharacters { get; set; } = EncapsulatingCharacters.Chevrons;
-
-        // Label Position
-        public enum LabelPosition
-        {
-            Before_Name,
-            After_Name
-        }
-
-        [SynthesisOrder]
-        [SynthesisSettingName("Label Position")]
-        [SynthesisTooltip("Where to put the label when creating the new name.")]
-        public LabelPosition labelPosition { get; set; } = LabelPosition.Before_Name;
     }
 }
